803f535a7afd0f01bd645e4edd4772607743d06e
usuario <usuario@DESKTOP-IAIA7KH>
2025-03-25 16:31:32 -0600

    Agregar archivos de proyecto.

---
+++ ServicioWindows/Models/Usuario.cs
@@ -0,0 +1,15 @@
+﻿namespace ServicioWindows.Models
+{
+    public class Usuario
+    {
+        public int ID { get; set; }
+        public string Nombre { get; set; }
+        public string ApellidoP { get; set; }
+        public string ApellidoM { get; set; }
+        public string Correo { get; set; }
+        public string FechaNac { get; set; }
+        public string FechaRegistro { get; set; }
+        public bool Activo { get; set; }
+        public string FechaMod { get; set; }
+    }
+}
---
+++ ServicioWindows/Services/UsuarioService.cs
@@ -0,0 +1,67 @@
+﻿using ServicioWindows.Utils;
+using ServicioWindows.Models;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using Newtonsoft.Json;
+using System.Net.Http;
+
+namespace ServicioWindows.Services
+{
+    public class UsuarioService
+    {
+        private string apiUrl = "http://localhost:52127/Services/Usuarios.svc/ObtenerUsuarios";
+        private string apiUrl2 = "http://localhost:52127/Services/UsuarioLogs.svc/ObtenerHistorialUsuarios";
+        public List<Usuario> ObtenerUsuarios()
+        {
+            try
+            {
+                using (WebClient client = new WebClient())
+                {
+                    client.Headers[HttpRequestHeader.ContentType] = "application/json";
+                    string json = client.DownloadString(apiUrl);
+
+                    var resultado = JsonConvert.DeserializeObject<dynamic>(json);
+                    string usuariosJson = resultado.ObtenerUsuariosResult.ToString();
+                    List<Usuario> usuarios = JsonConvert.DeserializeObject<List<Usuario>>(usuariosJson);
+
+                    Logger.EscribirLog($"Usuarios obtenidos: {usuarios.Count}");
+                    return usuarios;
+                }
+            }
+            catch (Exception ex)
+            {
+                Logger.EscribirLog($"Error al obtener usuarios: {ex.Message}");
+                return new List<Usuario>();
+            }
+        }
+
+        public List<UsuarioLog> ObtenerHistorialUsuarios()
+        {
+            try
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    HttpResponseMessage response = client.GetAsync(apiUrl2).Result;
+                    if (response.IsSuccessStatusCode)
+                    {
+                        string jsonResponse = response.Content.ReadAsStringAsync().Result;
+                        var resultado = JsonConvert.DeserializeObject<dynamic>(jsonResponse);
+                        return JsonConvert.DeserializeObject<List<UsuarioLog>>(resultado.ObtenerHistorialUsuariosResult.ToString());
+                    }
+                    else
+                    {
+                        Logger.EscribirLog("Error al obtener historial de usuarios.");
+                        return new List<UsuarioLog>();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Logger.EscribirLog($"Error en ObtenerHistorialUsuarios: {ex.Message}");
+                return new List<UsuarioLog>();
+            }
+        }
+
+    }
+}
