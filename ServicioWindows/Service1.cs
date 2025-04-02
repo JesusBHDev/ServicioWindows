0c10b951d8e3a0bbcda36edee6878700ab0ecc53
JesusBHDev <jesus.bh.dev@gmail.com>
2025-04-02 10:25:29 -0600

    Servicio en el que solo se utiliza la libreria DLL que se construyo

--- ServicioWindows/Service1.cs
+++ ServicioWindows/Service1.cs
@@ -1,89 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ServiceProcess;
-using System.Timers;
-using ServicioWindows.Models;
-using ServicioWindows.Services;
-using ServicioWindows.Utils;
+﻿using System.ServiceProcess;
+using LibreriaDSW;
+using LibreriaDSW.Utils;
 
 namespace ServicioWindows
 {
     public partial class Service1 : ServiceBase
     {
-        private Timer timer;
-        private UsuarioService usuarioService;
-        private CorreoService correoService;
-        private HashSet<int> usuariosFelicitadosHoy; // Almacena los IDs de usuarios ya felicitados
+        private ServicioUsuario ServicioUsuario;
 
         public Service1()
         {
             InitializeComponent();
-            usuarioService = new UsuarioService();
-            correoService = new CorreoService();
-            usuariosFelicitadosHoy = new HashSet<int>();
+            ServicioUsuario = new ServicioUsuario();
         }
 
         protected override void OnStart(string[] args)
         {
             Logger.EscribirLog("Servicio iniciado.");
-
-            timer = new Timer(60 * 1000); // Ejecutar cada minuto
-            timer.Elapsed += new ElapsedEventHandler(VerificarCumpleaños);
-            timer.Start();
-
-            VerificarCumpleaños(null, null);
+            ServicioUsuario.Iniciar();
         }
 
         protected override void OnStop()
         {
             Logger.EscribirLog("Servicio detenido.");
-            timer.Stop();
-        }
-
-        private void VerificarCumpleaños(object sender, ElapsedEventArgs e)
-        {
-            try
-            {
-                List<Usuario> usuarios = usuarioService.ObtenerUsuarios();
-                List<UsuarioLog> historial = usuarioService.ObtenerHistorialUsuarios();
-
-                if (usuarios.Count == 0)
-                {
-                    Logger.EscribirLog("No se encontraron usuarios.");
-                    return;
-                }
-
-                DateTime hoy = DateTime.UtcNow.Date;
-
-                foreach (var usuario in usuarios)
-                {
-                    if (DateTime.TryParse(usuario.FechaNac, out DateTime fechaNacimiento))
-                    {
-                        if (fechaNacimiento.Day == hoy.Day && fechaNacimiento.Month == hoy.Month)
-                        {
-                            // Verificar si ya se envió el correo hoy
-                            if (!usuariosFelicitadosHoy.Contains(usuario.ID))
-                            {
-                                correoService.EnviarCorreoConExcel(usuario.Correo, usuario.Nombre, historial);
-                                usuariosFelicitadosHoy.Add(usuario.ID);
-                                Logger.EscribirLog($"Correo con historial enviado a {usuario.Nombre} ({usuario.Correo})");
-                            }
-                            else
-                            {
-                                Logger.EscribirLog($"Correo ya enviado hoy a {usuario.Nombre}, no se enviará nuevamente.");
-                            }
-                        }
-                    }
-                    else
-                    {
-                        Logger.EscribirLog($"Error al convertir la fecha de nacimiento para {usuario.Nombre}.");
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                Logger.EscribirLog($"Error en VerificarCumpleaños: {ex.Message}");
-            }
+            ServicioUsuario.Detener();
         }
     }
 }
